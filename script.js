a06eace0ff19376c956b00919fe4bf933b175f56
Artem Bagaev <artemy.bagaev@gmail.com>
2019-04-23 18:54:11 +0300

    1. Добавил ластик 2. Сделал так, чтобы цвет и размер кисти менялся после изменения 3. Изменил сам стиль канваса. 4. Добавил возможность рисовать квадрат

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var ctx = canvas.getContext('2d');
 var xCoord = document.getElementById('xCoord');
 var yCoord = document.getElementById('yCoord');
 
+var colorVal = document.querySelector('#color');
+
 
 var getCoordinates = function (evt) {
 	let arr = {};
@@ -16,7 +18,7 @@ var getCoordinates = function (evt) {
 
 var system = {
 	currentTool : '',
-	currentColor : document.querySelector('#color'),
+	currentColor : colorVal,
 	brushSize : 5
 };
 
@@ -30,9 +32,12 @@ var switchTool = function (el) {
 	if (el.id == 'brush') {
 		console.log ('brush');
 		return 'brush'
-	} else if (el.id == 'notbrush') {
-		console.log ('not brush');
-		return 'notbrush'
+	} else if (el.id == 'cleaner') {
+		console.log ('cleaner');
+		return 'cleaner'
+	} else if (el.id == 'square') {
+		console.log ('square');
+		return 'square'
 	}
 };
 
@@ -40,28 +45,37 @@ var switchSize = function (list) {
 	return list.value
 };
 
+var switchColor = function(color) {
+	return color.value;
+}
+
 var clicker = function (evt) {
 	if (evt.target.classList.contains('toolButton') == true) {
 		//console.log (`Выбран инструмент ${evt.target.id}`);
 		//switchTool(evt.target);
 		console.log(system);
 		renderSystem (system, 'currentTool', switchTool (evt.target));
-	} else if (evt.target.id == 'sizeSelect') {
-		renderSystem (system, 'brushSize', switchSize(evt.target));
-	} else if (evt.target.id == 'color') {
-		//console.log (`Выбран инструмент color`);
 	}
 };
 
-
+var changer = function (evt) {
+	if (evt.target.id == 'sizeSelect') {
+		renderSystem (system, 'brushSize', switchSize(evt.target));
+	} else if (evt.target.id == 'color') {
+		renderSystem (system, 'currentColor', switchColor(evt.target));
+	}
+}
 
 var startDraw = function (evt) {
 	//зафиксировать нач коорд
 	//начать процесс рисования
 	if (system.currentTool == 'brush') {
 		draw (evt);
+	} else if (system.currentTool == 'cleaner') {
+		cleanBrush (evt);
+	} else if (system.currentTool == 'square') {
+		filler (evt);
 	}
-
 };
 
 var endDraw = function (evt) {
@@ -72,13 +86,29 @@ var endDraw = function (evt) {
 var draw = function (evt) {
 	canvas.onmousemove = function (evt) {
 		ctx.fillRect (xCoord.innerText, yCoord.innerText, system.brushSize, system.brushSize);
+		ctx.fillStyle = system.currentColor;
 	}
 };
+
+var cleanBrush = function (evt) {
+	canvas.onmousemove = function (evt) {
+		ctx.clearRect(xCoord.innerText, yCoord.innerText, system.brushSize, system.brushSize);
+	}
+};
+
+var filler = function (evt) {
+	canvas.onclick = function (evt) {
+		ctx.rect(xCoord.innerText, yCoord.innerText, 100, 100);
+		ctx.fillStyle = system.currentColor;
+		ctx.fill ();
+	}
+}
 
 canvas.addEventListener ('mousemove', getCoordinates);
 canvas.addEventListener ('mousedown', startDraw);
 canvas.addEventListener ('mouseup', endDraw);
 window.addEventListener ('click', clicker);
+window.addEventListener ('change', changer);
 
 var addZero = function (num) {
 	if (num <= 9) {
@@ -92,9 +122,10 @@ function start () {
 	window.setInterval(timer, 1000);
 }
 
-var canv = document.querySelector('.canv');
+var canv = document.querySelector('.coordinates');
 var dateElem = document.createElement('p');
-dateElem.class = 'clock';
+dateElem.className = 'clock';
+
 var date = new Date();
 var newDate = `${addZero(date.getHours())} : ${addZero(date.getMinutes())} : ${addZero(date.getSeconds())}`;
 dateElem.innerHTML = newDate;
